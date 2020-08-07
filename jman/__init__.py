25718af6504057a8436ca38bc8cbd75a0c961cc8
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-08-07 01:41:40 -0600

    jman/job.py: Add Job class that can spawn and execute an arbitrary module function.

---
+++ jman/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2020 by Terry Greeniaus.
+from .job import Job
+
+
+current_job = None
+
+
+__all__ = ['Job',
+           'current_job',
+           ]
---
+++ tests/job_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import time
+import sys
+
+from jman import Job, current_job
+
+
+def task(n):
+    for i in range(n):
+        current_job.set_meta({'count' : i})
+        time.sleep(1)
+
+
+def notify_meta(j):
+    print('Job %s: META updated to %s' % (j.uuid, j.meta))
+
+
+def notify_complete(j):
+    print('Job %s: completed with status %u' % (j.uuid, j.proc.returncode))
+
+
+if __name__ == '__main__':
+    j = Job('tests.job_test', 'task', 'TEST-TASK', args=(10,),
+            notify_meta=notify_meta, notify_complete=notify_complete)
+    j.spawn()
+    j.join()
